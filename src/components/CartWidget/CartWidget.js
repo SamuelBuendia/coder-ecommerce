42ec490a9f173ba091a67ae8827066cd5416ab5a
Samuel Buendia <sbuendiapuyo@gmail.com>
2021-04-18 02:12:56 -0500

    feat: add NavBar and CartWidget

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img  src="/assets/shoppingCartIcon.svg" width="25" height="auto" alt="la manzana de apple" />
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ItemListContainer = ({message}) => {
+    return (
+        <div>
+            <h1>
+                {message}
+            </h1>
+        </div>
+    )
+}
+
+export default ItemListContainer
